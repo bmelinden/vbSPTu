97e93ed3193c0513d3c80b13dbbff40e705b26ec
Martin Lindén <bmelinden@gmail.com>
2017-08-23 13:29:57 +0200

    basic mleYZdXtDs with state-dependent detachment/defocus rates d(s)

---
+++ +mleYZdXtDs/hiddenStateUpdate.m
@@ -0,0 +1,65 @@
+function [W,sMaxP,sVit,WS]=hiddenStateUpdate(W,dat)
+% [W,sMaxP,sVit,WS]=hiddenStateUpdate(W,dat) one round of hidden state EM
+% iteration (maximum likelihood) in a diffusive HMM with explicit
+% detachemtn rates, with possibly missing position data
+%
+% sMAxP and sVit are only computed if asked for by output arguments.
+% WS : struct containing the whole workspace at the end of the function
+% call. Expensive, computed only when asked for.
+
+% v1: modified from EMhmm version, checked correctness (not perfect, but
+% good enough to blame on model differences...).
+% v2: optimized computing lnH by getting rid of the trj loop
+
+
+%% start of actual code
+tau=W.shutterMean;
+R=W.blurCoeff;
+beta=tau*(1-tau)-R;
+%% assemble point-wise weights
+T=W.YZ.i1(end);
+Ntrj=numel(W.YZ.i1);
+lnH=-W.dim*ones(T,1)*log(W.P.lambda);
+lnH(W.YZ.i0,:)=lnH(W.YZ.i0,:)+ones(length(W.YZ.i0),1)*log(W.P.p0);
+lnH=lnH-sum(...
+        [diff(W.YZ.muY).^2 ;zeros(1,W.dim)] ...
+        +1/beta*(W.YZ.muZ-(1-tau)*W.YZ.muY-tau*W.YZ.muY([2:end end],:)).^2 ...
+        +(1+(1-tau)^2/beta)*W.YZ.varY ...
+        +(1+    tau^2/beta)*W.YZ.varY([2:end end],:) ...
+        +1/beta*W.YZ.varZ...
+        +2*R/beta*W.YZ.covYtYtp1...
+        -2*(1-tau)/beta*W.YZ.covYtZt...
+        -2*tau/beta*W.YZ.covYtp1Zt...
+        ,2)/2*(1./W.P.lambda);
+% detatchment rate contributions
+lnD=             zeros(T   ,W.numStates)+log(1-W.P.d);
+lnD(W.YZ.i1-1,:)=zeros(Ntrj,W.numStates)+log(W.P.d  );
+lnD(W.YZ.i1,:)=0;
+lnH=lnH+lnD;
+lnH(W.YZ.i1,:)=0;
+lnHmax=max(lnH,[],2);
+lnH=lnH-lnHmax*ones(1,W.numStates);
+H=exp(lnH);
+H(W.YZ.i1,:)=0;
+%% forward-backward iteration
+%[ln3,wA3,ps3]=HMM_multiForwardBackward_g1(W.P.A,H,dat.i1);
+[lnZ,W.S.wA,W.S.pst]=HMM_multiForwardBackward_startend(W.P.A,H,dat.i0,dat.i1);
+W.S.lnZ=lnZ+sum(lnHmax);
+
+%% likelihood lower bound after s
+W.lnL=W.S.lnZ+W.YZ.Fs_yz;
+%% path estimates
+if(nargout>=2) % compute sequence of most likely states
+    [~,sMaxP]=max(W.S.pst,[],2);
+    sMaxP(W.YZ.i1)=0;
+end
+if(nargout>=3) % compute Viterbi path, with a small offset to avoid infinities
+    %%%sVit=HMM_multiViterbi_log_startend(log(W.P.A+1e-50),log(H+1e-50),W.YZ.i0,W.YZ.i1-1);
+    error('viterbi path with detachment rates not implemented for mleYZdXtDs yet')
+end
+if(nargout>=4)
+   fname=['foo_' int2str(ceil(1e5*rand)) '.mat'];
+   save(fname);
+   WS=load(fname);
+   delete(fname);
+end
---
+++ +mleYZdXtDs/parameterUpdate.m
@@ -0,0 +1,56 @@
+function W=parameterUpdate(W,~)
+% A parameter update iteration (MLE) on a diffusive YZdXtDs HMM, including
+% handling missing data 
+
+%% start of actual code
+
+tau=W.shutterMean;
+R=W.blurCoeff;
+beta=tau*(1-tau)-R;
+
+%% parameter update
+W.P.A =rowNormalize(W.S.wA);
+W.P.p0=rowNormalize(sum(W.S.pst(W.YZ.i0,:),1));
+
+W.P.d = sum(W.S.pst(W.YZ.i1-1,:),1)./sum(W.S.pst,1);
+
+% check for unoccupied rows, and add a minimal pseduocount to them
+wAemptyRows=find((sum(W.S.wA,2)==0))';
+if(~isempty(wAemptyRows)) % a very non-invasive regularization, no new transitions
+   W.P.A=rowNormalize(W.S.wA+10*eps*eye(W.numStates));
+   W.P.p0=rowNormalize(sum(W.S.pst(W.YZ.i0,:),1)+10*eps);
+   %warning(['State(s) ' int2str(wAemptyRows) ' unoccupied, MLEparameterUpdate adding 10*eps pseudocounts to avoid NaNs'])
+end
+
+% index to all hidden states, including missing data 
+indS=1:W.YZ.i1(end);
+indS(W.YZ.i1)=0;
+indS=indS(indS>0); 
+
+sumDim_dY2_dYZ2=sum(...
+    (W.YZ.muY(indS+1,:)-W.YZ.muY(indS,:)).^2 ...
+    +1/beta*(W.YZ.muZ(indS,:)-(1-tau)*W.YZ.muY(indS,:)-tau*W.YZ.muY(indS+1,:)).^2 ...
+    +(1+(1-tau)^2/beta)*W.YZ.varY(indS,:) ...
+    +(1+tau^2/beta)*W.YZ.varY(indS+1,:) ...
+    +1/beta*W.YZ.varZ(indS,:)...
+    +2*R/beta*W.YZ.covYtYtp1(indS,:)...
+    -2*(1-tau)/beta*W.YZ.covYtZt(indS,:)...
+    -2*tau/beta*W.YZ.covYtp1Zt(indS,:)...
+    ,2);
+c=zeros(1,W.numStates);
+for k=1:W.numStates
+    c(k)=0.5*sum(W.S.pst(indS,k).*sumDim_dY2_dYZ2);
+end
+n=W.dim*sum(W.S.pst(indS,:),1); 
+
+% new for YZhmm: analytical maximum for diffusion step variance!
+W.P.lambda=c./n;
+
+% deal with unoccupied states
+iNull=find(n==0);
+if(~isempty(iNull))
+    W.P.lambda(iNull)=1e100; % ridiculously large value to put these constants last in ordered models
+    warning(['YZhmm generated unoccupied states : ' int2str(iNull) ])
+end
+
+
