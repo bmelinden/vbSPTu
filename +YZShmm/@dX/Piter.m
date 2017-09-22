841cb33a45b61ee92d4ed7ebeabe3bdfe2cb69be
Martin Lindén <bmelinden@gmail.com>
2017-09-22 23:30:32 +0200

    KL and lnP fields

--- +YZShmm/@dX/Piter.m
+++ +YZShmm/@dX/Piter.m
@@ -16,6 +16,8 @@ switch lower(iType)
         this.P.nv=sum(nv(:));        
         % no KL terms in mle updates
         this.P.KL.pi=0;this.P.KL.a=0;this.P.KL.B=0;this.P.KL.lambda=0;this.P.KL.v=0;
+        % no log-prior terms in mle updates
+        this.P.lnP0.pi=0;this.P.lnP0.a=0;this.P.lnP0.B=0;this.P.lnP0.lambda=0;this.P.lnP0.v=0;
     case 'map'
         this.P.wPi=this.P0.wPi+wPi;
         this.P.wa =this.P0.wa+wa;
@@ -27,6 +29,10 @@ switch lower(iType)
         this.P.nv=this.P0.nv+sum(nv(:));
         % no KL terms in map updates
         this.P.KL.pi=0;this.P.KL.a=0;this.P.KL.B=0;this.P.KL.lambda=0;this.P.KL.v=0;
+        % compute log-prior terms
+        [this.P.lnP0.pi,this.P.lnP0.a,this.P.lnP0.B,this.P.lnP0.lambda]=YZShmm.P0AD_lnP0terms(this.P,this.P0);
+        v=this.P.cv./(this.P.nv+1);
+        this.P.lnP0.v=this.P0.nv.*log(this.P0.cv)-gammaln(this.P0.nv)-(this.P0.nv-1).*log(v)-this.P0.cv./v;
     case 'vb'
         this.P.wPi=this.P0.wPi+wPi;
         this.P.wa =this.P0.wa+wa;
@@ -35,13 +41,16 @@ switch lower(iType)
         this.P.c  =this.P0.c+c;
         % lump stats for all dimensions and states        
         this.P.cv=this.P0.cv+sum(cv(:));
-        this.P.nv=this.P0.nv+sum(nv(:));        
-        [this.P.KL.pi,this.P.KL.a,this.P.KL.B,this.P.KL.lambda]=YZShmm.P0AD_KLterms(this.P,this.P0);        
-        
+        this.P.nv=this.P0.nv+sum(nv(:));
+        % no log-prior terms in vb updates
+        this.P.lnP0.pi=0;this.P.lnP0.a=0;this.P.lnP0.B=0;this.P.lnP0.lambda=0;this.P.lnP0.v=0;
+        % compute KL terms
+        [this.P.KL.pi,this.P.KL.a,this.P.KL.B,this.P.KL.lambda]=YZShmm.P0AD_KLterms(this.P,this.P0);                
         this.P.KL.v= this.P0.nv.*log(this.P.cv./this.P0.cv)...
             -this.P.nv.*(1-this.P0.cv./this.P.cv)...
             -gammaln(this.P.nv)+gammaln(this.P0.nv)...
             +(this.P.nv-this.P0.nv).*psi(this.P.nv);
+        
     case 'none'
         return
     otherwise
