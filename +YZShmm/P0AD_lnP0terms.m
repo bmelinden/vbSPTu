b5aa569a950f3789809b9a22b34ca0924bb4fa43
Martin Lindén <bmelinden@gmail.com>
2017-09-26 23:18:46 +0200

    MAP log-priors for old matlab versions

--- +YZShmm/P0AD_lnP0terms.m
+++ +YZShmm/P0AD_lnP0terms.m
@@ -18,10 +18,22 @@ Lambda = P.c./(P.n+1);
 %% log(prior) terms
 % 'omitnan' in the last term because 0*log(0)=nan in matlab, but we
 % want the limit log(0^0)=log(1)=0
-lnP0_pi=    gammaln(sum(P0.wPi,2))-sum(gammaln(P0.wPi),2)+sum(lnp0.*(P0.wPi-1),'omitnan'); % p0-log prior
-lnP0_a =gammaln(sum(P0.wa ,2))-sum(gammaln(P0.wa) ,2)+sum(log(a).*(P0.wa-1),2,'omitnan');
-% special construct so that the diagonal B-terms do not contribute,
-lnP0_B =gammaln(sum(P0.wB, 2))-sum(gammaln(P0.wB+1-B1),2)+sum(log(B+I1).*(P0.wB-B1),2,'omitnan');
+try
+    lnP0_pi=    gammaln(sum(P0.wPi,2))-sum(gammaln(P0.wPi),2)+sum(lnp0.*(P0.wPi-1),'omitnan'); % p0-log prior
+    lnP0_a =gammaln(sum(P0.wa ,2))-sum(gammaln(P0.wa) ,2)+sum(log(a).*(P0.wa-1),2,'omitnan');
+    % special construct so that the diagonal B-terms do not contribute,
+    lnP0_B =gammaln(sum(P0.wB, 2))-sum(gammaln(P0.wB+1-B1),2)+sum(log(B+I1).*(P0.wB-B1),2,'omitnan');
+catch me
+    % ignore 'omitnan' on older versions of matlab
+   if(strcmp(me.identifier,'MATLAB:sum:unknownFlag'))
+    lnP0_pi=    gammaln(sum(P0.wPi,2))-sum(gammaln(P0.wPi),2)+sum(lnp0.*(P0.wPi-1)); % p0-log prior
+    lnP0_a =gammaln(sum(P0.wa ,2))-sum(gammaln(P0.wa) ,2)+sum(log(a).*(P0.wa-1),2);
+    % special construct so that the diagonal B-terms do not contribute,
+    lnP0_B =gammaln(sum(P0.wB, 2))-sum(gammaln(P0.wB+1-B1),2)+sum(log(B+I1).*(P0.wB-B1),2);
+   else
+       retrow(me)
+   end    
+end
 lnP0_lambda=P0.n.*log(P0.c)-gammaln(P0.n)-(P0.n-1).*log(Lambda)-P0.c./Lambda;
 %% control for finite log-prior terms
 if( ~isfinite(sum(lnP0_pi)) || ~isfinite(sum(lnP0_a)) || ~isfinite(sum(lnP0_B)) || ~isfinite(sum(lnP0_lambda)) )
