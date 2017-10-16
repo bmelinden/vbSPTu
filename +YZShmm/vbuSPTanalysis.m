f5325d0e0856ebb24ff4ae2aa503a1a7582fbf46
Martin Lindén <bmelinden@gmail.com>
2017-10-16 11:08:55 +0200

    first +YZShmm/vbuSPTanalysis.m debug, and added help text

--- +YZShmm/vbuSPTanalysis.m
+++ +YZShmm/vbuSPTanalysis.m
@@ -2,6 +2,65 @@ function res=vbuSPTanalysis(runinput)
 % res=YZShmm.vbuSPTanalysis(runinput)
 % Run an uSPT HMM analysis pipeline based on options struct parameters or
 % runinput file.
+%
+% runinput  : name of runinput file, or option struct. The runinput file
+%             can be produced by the usptGUI, or written by hand. See
+%             comments in the example runinput file YZSruninput.m for a
+%             description of the various options.
+%
+% res       : a struct with the results of the analysis, which is also
+%             saved to the file specified in opt.output.outputFile. Fields:
+% 
+%  	runinput    : the runinput input 
+% 	opt         : the options struct produced by runinput
+%   X           : preprocessed data
+% --- selected model 
+%   Wbest       : selected model, by the variational Bayes (VB) or
+%                 pseudo-Bayes factor (PBF) criterion, depending on
+%                 opt.modelSearch.PBF. If opt.modelSearch.MLEparam=true,
+%                 Wbest is a converged maximum likelihood estimate
+%                 (otherwise variational Bayes).
+%   Nbest       : number of states in Wbest.
+% 	Pbest       : estimated parameters in Wbest (VB or MLE).
+% --- VB model search results from YZShmm.VBmodelSearchVariableSize
+%   VBbestN     : The best VB models of all sized encountered during the VB
+%                 model search, up to opt.modelSearch.maxHidden
+%   VB_dlnL     : log evidence lower bounds of VBbestN, relative to the
+%                 best model
+%   VB_INlnL    : Iteration-, model size, and lnL- value for each model
+%                 encountered during the greedy search. 
+%   VB_P        : Model parameters for each entry in VB_INlnL
+% --- Pseuao-Bayes factor (PBF) results
+%   PBF_H       : raw PBF from cross-validation, rescaled to the whole data
+%                 set size. 
+%   PBF_dlnL    : Mean PBF over all cross-validation instances, and offset
+%                 relative to the best model
+%   PpBF_dlnLstdErr: standard error (standard deviation / sqrt(number of
+%                 cross-validations) ) for PBF_dlnL.
+% --- bootstrap parameters: bootstrap resampling is done by at the
+%                 trajectory level so that the resampled data sets have the
+%                 same number of trajectories (but not necessarily the
+%                 exact same number of positions) as the original data X.
+%   PbestBS     : bootstrap parameters for Wbest (VB or MLE, depending on
+%                 opt.modelSearch.MLEparam), organized so that
+%                 PbestBS.XXX(:,:,j) is the estimated parameters from
+%                 bootstrap sample j.
+%   PbestBSstd  : bootstrap standard deviation of the estimated parameters,
+%                 can be used as an estimate of parameter standard error.
+% --- bootstrap model selection
+%	VB_lnLbs    : relative VB log evidence lower bound for the VBbestN
+%                 models for each bootstrap sample.
+%   NVBbs       : VB-selected number of states for each bootstrap sample.
+%
+%   PBF_Hbs     : rescaled PBF. Here, bootstrapping is instead done
+%                 directly on PBF_H without reconverging any model on
+%                 bootstrapped trajectories.
+%   NPBFbs      : number of states selected by the cross-validated PBF.
+%
+% The distributions of NVBbs and NPBFbs can be used to estimated the
+% statistical uncertainty in the model selection.
+%
+% ML 2017-10-16
 
 %% print copyright message
 
@@ -57,10 +116,11 @@ if(opt.bootstrap.modelSelection)
         [~,NPBFbs]=max(PBF_Hbs,[],2);
     end
     % bootstrap VB model selection
-    [~,~,~,~,VB_lnLbs]=bootstrap(VBbestN,X,'vb',opt.bootstrap.bootstrapNum,'Dsort',true,'displayLevel',1);
+    [~,~,~,~,VB_lnLbs]=YZShmm.bootstrap(VBbestN,X,'vb',opt.bootstrap.bootstrapNum,'Dsort',true,'displayLevel',1);
     %%% got this far!!!
     [~,NVBbs]=max(VB_lnLbs,[],2);
 end
 %% write results to file
+clear ans 
 save(opt.output.outputFile);
 res=load(opt.output.outputFile);
